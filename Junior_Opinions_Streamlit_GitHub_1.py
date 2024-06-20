31e642eaa7008dc107ccb9de5967a14e936aadfe
cplooo <cplo@kimo.com>
2024-06-20 21:37:16 +0800

    Add files via upload

--- Junior_Opinions_Streamlit_GitHub_1.py
+++ Junior_Opinions_Streamlit_GitHub_1.py
@@ -1308,7 +1308,7 @@ with st.expander("2-6-1.工讀的原因(不列計沒有工讀):"):
 
     
     ##### 產出 result_df: 加條件: 
-    ranking_number = 1
+    ranking_number = 2
     
     # if 系_院_校 == '0':
     #     df_junior_restrict = df_junior.dropna(subset=[df_junior.columns[column_index]])
