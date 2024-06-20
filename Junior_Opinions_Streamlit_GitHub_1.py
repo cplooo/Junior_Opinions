3e3f6691758a031dbe18cedabd5de6ce4b356770
cplooo <cplo@kimo.com>
2024-06-20 17:53:28 +0800

    Add files via upload

--- Junior_Opinions_Streamlit_GitHub_1.py
+++ Junior_Opinions_Streamlit_GitHub_1.py
@@ -29,7 +29,7 @@ def load_data(path):
 def Frequency_Distribution(df, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1,row_rank=False, row_rank_number=3): ## 當有去掉dropped_string & 是單選題時, sum_choice 要使用 0
     ##### 使用 dropna 方法去掉包含 NaN 的所有 rows
     df = df.dropna()  ## 預設是去掉有NA的row,如果是行,要設定axis=1.
-    if row_rank=True:
+    if row_rank==True:
         ##### 使用 str.split 方法分割第14行的字串，以 ';' 為分隔符, 然後使用 apply 和 lambda 函數來提取前三個元素, 並再度以;分隔.
         # df_junior['col14'] = df_junior['col14'].str.split(';').apply(lambda x: ';'.join(x[:3]))
         df.iloc[:,column_index] = df.iloc[:,column_index].str.split(split_symbol).apply(lambda x: ';'.join(x[:row_rank_number]))
